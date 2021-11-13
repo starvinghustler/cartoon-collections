77303fe05fc8d4db61a57de43f3b9e58ed73695e
idobok <idobok12@gmail.com>
2021-11-13 17:22:51 +0200

    commit tree d125af14bced5416d980c9b7a4e9d73dcf69baf4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+group = ["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Sleepy", "Happy"]
+
+def roll_call_dwarves(group)# code an argument here
   # Your code here
+  group.each_with_index do |dwarf, index|
+    puts "#{index +1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+calls =  ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(calls)
+  calls.map { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+array = ["shoe", "rope", "gouda"]
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
